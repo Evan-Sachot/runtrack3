ff62333dd9dad2005f2daffef1efc237b80853ed
evan-sachot <evan.sachot@laplateforme.io>
2025-07-07 16:42:24 +0200

    job 07

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,3 +1,48 @@
-function jourtravaille(){
-    
+function jourtravaille(date){
+
+const joursferie = [
+    Date("2020-12-12"),
+    Date("2020-02-07"),
+    Date("2020-01-07"),
+]
 }
+
+prompt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function jourtravaille(input){
+//  let jours=input;
+//  let mois1=input;
+//  let answer;
+//  if (jours + mois1 == "2","17","13","27","21","23","22","37"){
+//   answer = "yes";
+//  }
+//  else {
+// answer="no";
+//  }
+ 
+// alert(answer);
+// }
+
+
+// var joursferie = ["2","17","13","27","21","23","22","37"]
+
+// const mois = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Jin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+
+// let jours=parseInt(prompt("enter a value",0),31);
+// let mois1=parseInt(prompt("enter a value",0),31);
+// jourtravaille()
---
+++ jour01/job08/script.js
@@ -0,0 +1,5 @@
+function sommenombrespremiers(){
+ 
+}
+var a=parseInt(prompt(""));
+var b = parseInt(prompt(""));
