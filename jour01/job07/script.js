bf8941dec131f14edb5e5fb9cca7ec2e49c0dab8
evan-sachot <evan.sachot@laplateforme.io>
2025-07-08 10:34:33 +0200

    jour 07

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,48 +1,28 @@
 function jourtravaille(date){
-
-const joursferie = [
-    Date("2020-12-12"),
-    Date("2020-02-07"),
-    Date("2020-01-07"),
+    // date = new Date();
+}
+const jourferie = [
+    new Date(Date.UTC(2020, 3, 13)),
+    new Date(Date.UTC(2020, 0, 1))
 ]
+
+
+
+var a = new Date(prompt("date 20XX-MM-DD"));
+
+for (var i=0; i < jourferie.length; i++) {
+
+if (a <= jourferie[i]) 
+{
+    console.log("jour ferié");
+} 
+else 
+{
+    console.log("jour travaillé");
 }
 
-prompt
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function jourtravaille(input){
-//  let jours=input;
-//  let mois1=input;
-//  let answer;
-//  if (jours + mois1 == "2","17","13","27","21","23","22","37"){
-//   answer = "yes";
-//  }
-//  else {
-// answer="no";
-//  }
- 
-// alert(answer);
-// }
-
-
-// var joursferie = ["2","17","13","27","21","23","22","37"]
-
-// const mois = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Jin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
-
-// let jours=parseInt(prompt("enter a value",0),31);
-// let mois1=parseInt(prompt("enter a value",0),31);
-// jourtravaille()
+console.log(a);
+console.log(jourferie); 
+jourtravaille();
