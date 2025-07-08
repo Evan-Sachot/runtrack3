a5bf01671e25db556450b74f4b328a60d39a226d
evan-sachot <evan.sachot@laplateforme.io>
2025-07-08 12:00:27 +0200

    Update script.js

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,4 +1,4 @@
-function sommenombrespremiers(){
+function sommenombrespremiers(a,b){
 for (i=2; i < a; i++){
 if (a%i == 0 || b%i == 0) {
 return false;
@@ -14,5 +14,5 @@ else{
 
 let a = parseInt(prompt());
 let b = parseInt(prompt());
-console.log(sommenombrespremiers());
+console.log(sommenombrespremiers(a,b));
 sommenombrespremiers();
