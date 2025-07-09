d27928f8764584386ec01838a000933480b053b8
evan-sachot <evan.sachot@laplateforme.io>
2025-07-09 10:55:19 +0200

    Update script.js

--- jour02/job02/script.js
+++ jour02/job02/script.js
@@ -1,5 +1,6 @@
-function showhide(){
 let x = document.getElementById("article");
+x.style.display ="none";
+function showhide(){
 if(x.style.display === "none"){
 x.style.display = "block";
 }else{
