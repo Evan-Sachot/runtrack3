e08e48ee3e953c231eb298ae9a192d0e7ce87d7d
evan-sachot <evan.sachot@laplateforme.io>
2025-07-09 10:30:29 +0200

    job01

---
+++ jour02/job01/script.js
@@ -0,0 +1,7 @@
+const elementCitation = document.getElementById("citation").textContent ;
+document.getElementById("button").addEventListener("click",citation);
+function citation(content){
+    
+console.log(elementCitation);
+
+}
