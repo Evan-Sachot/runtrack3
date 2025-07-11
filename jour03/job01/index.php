e2b1327bbcb564eeae1d2350704e6a74c5bb9b3d
evan-sachot <evan.sachot@laplateforme.io>
2025-07-11 09:56:15 +0200

    jquerry

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="script.js"></script>
 
 </head>
 <body>
-    <article id="citation">La vie a beacoup plus d'imagination que nous.</article>
-    <button onclick="showhide()">citation console</button>
-   <script src="script.js"></script>
+    <p>Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+ensuite on prie.</p>
+    <button id="button1">citation console</button>
+   
 </body>
 </html>
