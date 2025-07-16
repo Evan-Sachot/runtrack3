8855bc8342abd0ada8484fe87517c4f196aa9af1
evan-sachot <evan.sachot@laplateforme.io>
2025-07-16 15:26:15 +0200

    ok

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -8,6 +8,25 @@
 <body>
     <input type="text" id="nameinput" name="Nom" >
     <input type="number" id="id" name="id">
+    <select name="type" id="type">
+     <option value="">Select an option</option>
+            <option value="Grass">Grass</option>
+            <option value="Poison">Poison</option>
+            <option value="Fire">Fire</option>
+            <option value="Flying">Flying</option>
+            <option value="Water">Water</option>
+            <option value="Bug">Bug</option>
+            <option value="Normal">Normal</option>
+            <option value="Electric">Electric</option>
+            <option value="Ground">Ground</option>
+            <option value="Fairy">Fairy</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Psychic">Psychic</option>
+            <option value="Rock">Rock</option>
+            <option value="Steel">Steel</option>
+            <option value="Ice">Steel</option>
+            <option value="Dragon">Dragon</option>   
+         </select>
     <button id="filtrer">Filtrer</button>
 
     <script src="script.js"></script> 
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,27 +1,9 @@
-
-
-$(document).ready(function(){
-function jsonValueKey(json,key){
-return json[key]
+async function afficheJson(path){
+const response = await fetch (path)
+return await response.json()
 }
-$("#filtrer").click(function(){
-let name = document.getElementById("nameinput").value
-let id = document.getElementById("id").value
 
-fetch("pokemon.json").then(
-function(u){return u.json();}
-).then
-(
-function(json){
-data = json;
-if (name === undefined)
-var item = data.find(item => item.name.english || item.name.french || item.name.chinese || item.name.japanese  === name);
-item = data.find(item => item.id === id)
-console.log(item.name);
-console.log(item.id)
-console.log(item.type)
-}
+const json = afficheJson(
+'http://localhost/runtrack3/jour04/job03/pokemon.json',
 )
-
-})
-})
+ 
