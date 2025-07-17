3c391e6e0fa85076cbf1d52afa52b94fde71f09b
evan-sachot <evan.sachot@laplateforme.io>
2025-07-17 13:59:45 +0200

    C

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -3,23 +3,23 @@ $(document).ready(function(){
     
     $("#randomize").click(function(){
       $('ul').each(function(){
-            // get current ul
+           
             var $ul = $(this);
-            // get array of list items in current ul
+            
             var $liArr = $ul.children('li');
-            // sort array of list items in current ul randomly
+          
             
             $liArr.sort(function(a,b){
-                  // Get a random number between 0 and 10
+                 
                   var temp = parseInt( Math.random()*10);
-                  // Get 1 or 0, whether temp is odd or even
+                  
                   var isOddOrEven = temp%2;
-                  // Get +1 or -1, whether temp greater or smaller than 5
+                  
                   var isPosOrNeg = temp>5 ? 1 : -1;
-                  // Return -1, 0, or +1
+                 
                   return( isOddOrEven*isPosOrNeg );
             })
-            // append list items to ul
+            
             .appendTo($ul);      
         }) 
        
@@ -35,11 +35,11 @@ $(document).ready(function(){
         })    
         $("#buttonreset").click(function(){
         $('ul').each(function(){
-            // get current ul
+            
             var $ul = $(this);
-            // get array of list items in current ul
+            
             var $liArr = $ul.children('li');
-            // sort array of list items in current ul randomly
+            
             
             $liArr.sort(function(a,b){
                 return a.id - b.id
