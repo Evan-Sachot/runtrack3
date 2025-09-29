99ff5f805b53335b2c82cce069ac7ef61d9ba832
evan-sachot <evan.sachot@laplateforme.io>
2025-09-29 09:24:33 +0200

    Update index.php

---
+++ jour05/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+</head>
+<body>
+  <div class="board" id="board">
+  <img src="1.PNG" alt="">
+  <img src="2.PNG" alt="">
+  <img src="3.PNG" alt="">
+  <img src="4.PNG" alt="">
+  <img src="5.PNG" alt="">
+  <img src="6.PNG" alt="">
+  <img src="7.PNG" alt="">
+  <img src="8.PNG" alt="">
+  <img src="vide.png" alt="">
+</div>
+</body>
+<script src="script.js"></script>
+</html>
